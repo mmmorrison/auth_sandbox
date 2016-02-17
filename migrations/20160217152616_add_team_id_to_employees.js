e624e57f3d79a783743921f9b4e6f77d7486e102
Maria <mmorrison1978@gmail.com>
2016-02-17 15:31:59 -0700

    added team_id to empoyee table

---
+++ migrations/20160217152616_add_team_id_to_employees.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table('employees', function (t) {
+    t.integer('team_id');
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('employees',function (t) {
+      t.dropColumn('team_id')
+    });
+};
